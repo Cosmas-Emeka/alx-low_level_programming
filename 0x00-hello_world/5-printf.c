8bc5110846499ac3b8568c9884484b64c00f287c
Cosmas-Emeka <cosmasemeka11@gmail.com>
2022-10-14 00:27:14 +0100

    displaying output with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to program
+ *
+ * Description - Using printf to display output
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
