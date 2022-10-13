4ead62e9a7ab3f86f86dabbc54ed50e1fd43508d
Cosmas-Emeka <cosmasemeka11@gmail.com>
2022-10-13 19:40:42 -0400

    Displaying output with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point to program
  *
- * Description - Using printf to display output
+ * Description: Using printf to display output
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
