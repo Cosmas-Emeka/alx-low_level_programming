ba74db16f3ed7d84038ac9be67eb7077b651c3a3
Cosmas-Emeka <cosmasemeka11@gmail.com>
2022-10-13 20:02:59 -0400

    Displaying various size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,20 +3,19 @@
 /**
  * main - Function computes sizes of various data types
  *
- * Description: to display size of char, int, long int, long long int, and float 
+ * Description: Using sizeof to display size of char, int, long int, long long int, and float 
  * 
- * Return: zero
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char c;
-	int i;
-	float f;
 
 	printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
 	printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
 	printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte\(s\)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte\(s\)\n", sizeof(f));
+
 	return (0);
+
 }
