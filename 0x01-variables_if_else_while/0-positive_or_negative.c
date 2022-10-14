228e775b1b560a474660be1becba222efdc98f88
Cosmas-Emeka <cosmasemeka11@gmail.com>
2022-10-14 18:42:15 -0400

    Conditional statement on poitive and negative int

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
-/* main - Entry point
- *
- * Description: displaying negative or postive value of n
- *
- * Return: Always 0 (Success)
- */
+/**
+*main - entry point of programs
+*
+*Return: return 0
+*/
 
-        int main(void)
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 2)
-		printf("%d is zero\n", n);
-	else
+	if (n < 0)
+	{
 		printf("%d is negative\n", n);
-
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
