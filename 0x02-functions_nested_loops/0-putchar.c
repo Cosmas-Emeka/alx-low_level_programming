6bdd0c5fa7a3af9acdb9277daabd68179e31257d
Cosmas-Emeka <cosmasemeka11@gmail.com>
2022-10-18 11:24:34 -0400

    A readme and putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point of program
- *
- * Description - A program that prints the text _putchar
- *
- * Return: Always 0 (Success)
- */
+*main - function prints text as output
+*
+*Return:return 0
+*/
 
 int main(void)
 {
-	printf("_putchar\n");
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
 	return (0);
 }
