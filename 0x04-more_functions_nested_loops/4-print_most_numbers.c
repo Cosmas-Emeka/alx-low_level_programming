f176e4a1523dd0c99461442e62467da77898c26c
Cosmas-Emeka <cosmasemeka11@gmail.com>
2022-10-21 15:33:18 +0100

    Resolving task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
 *print_most_numbers - prints numbers except 2 and 4
 *
 *Return: returns nothing
 */
+
 void print_most_numbers(void)
 {
 int number;
@@ -16,3 +18,4 @@ continue;
 _putchar(number);
 }
 _putchar(10);
+}
